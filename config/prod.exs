e7515dc435b6988585cafcf3759a3e07abd8d017
manuelgeek <emashmagak@appslab.co.ke>
2020-02-25 22:26:02 +0300

    added heroku link

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :shop, ShopWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "rocky-depths-40660.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "phx-shop.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
