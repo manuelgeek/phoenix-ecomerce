79cefa961544cfc82962ea437b7a76ff099fda61
manuelgeek <emashmagak@appslab.co.ke>
2020-03-01 16:38:29 +0300

    added mpesa .onfig

--- config/prod.exs
+++ config/prod.exs
@@ -61,3 +61,13 @@ config :shop, Shop.Repo,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 # import_config "prod.secret.exs"
+
+# on prod, will definatly use .env
+config :shop, ShopWeb.Endpoint,
+  mpesa: [
+    consumer_key: "72yw1nun6g1QQPPgOsAObCGSfuimGO7b",
+    consumer_secret: "vRzZiD5RllMLIdLD",
+    mpesa_short_code: "174379",
+    mpesa_passkey: "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",
+    mpesa_callback_url: "http://91eb0af5.ngrok.io/api/payment/callback"
+  ]
