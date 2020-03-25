7513f7aa7e7059e840d6911e4cc656ecc4c6f2b3
manuelgeek <emashmagak@appslab.co.ke>
2020-03-25 18:48:23 +0300

    added seeds2.exs to ecto.setup

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule Shop.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs", "run priv/repo/seeds2.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
